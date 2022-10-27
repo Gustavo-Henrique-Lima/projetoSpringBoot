2e7f5eb22bc709d34416299de92c4d8ee59e28a2
Gustavo Henrique <gustavohgustavo@hotmail.com>
2022-10-27 18:30:27 -0300

    Criação da documentação

--- src/main/java/com/gustavonascimento/projetoSpring/config/TestConfig.java
+++ src/main/java/com/gustavonascimento/projetoSpring/config/TestConfig.java
@@ -66,7 +66,7 @@ public class TestConfig implements CommandLineRunner{
 		
 		produtoRepositorio.saveAll(Arrays.asList(pr1,pr2,pr3,pr4,pr5));
 		
-		Usuario u1=new Usuario(null, "Gustavo","gustavo@gmail.com", "8199", "123");
+		Usuario u1=new Usuario(null, "Pedro Freitas","gustavo@gmail.com", "8199", "123");
 		Usuario u2=new Usuario(null, "Guilherme", "guilherme@gmail.com", "81985", "321");
 		Pedido p1= new Pedido(null, Instant.parse("2022-10-02T11:45:08Z"),StatusDoPedido.AGUARDANDO_PAGAMENTO ,u1);
 		Pedido p2= new Pedido(null, Instant.parse("2022-10-01T11:30:45Z"), StatusDoPedido.ENVIADO,u2);
